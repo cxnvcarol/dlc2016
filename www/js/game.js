ec43ef04831f383569645aaa2a5063f999151e9b
Max Zeyen <m_zeyen09@cs.uni-kl.de>
2016-03-05 13:42:28 +0100

    added follow function for quests and finish criteria.

--- www/js/game.js
+++ www/js/game.js
@@ -33,17 +33,46 @@ var Quest = {
         // based on interests and available time select quest/target
         return target;
     },
-    // track the position of the current quest
-    follow: function(target, currentPosition) {
-        // get direction to target based on current position
-        // get current position
-        // return normalized 2d direction vector
-        return direction;
+    // track the position of the current quest and draw an arrow on map in radius l and with width u.
+    follow: function(target, currentPosition, u, l) {
+        var normalize = function(vec) {
+            var len = Math.sqrt(vec.long * vec.long + vec.lati * vec.lati);
+            return {
+                long:vec.long / len,
+                lati:vec.lati / len
+            };
+        };
+        var direction = {
+            long: target.long - currentPosition.long,
+            lati: target.lati - currentPosition.lati
+        };
+        direction = normalize(direction);
+        var normal = {
+            long: direction.lati,
+            lati: - direction.long
+        };
+        var p = {
+            long: currentPosition.long + l * direction.long,
+            lati: currentPosition.lati + l * direction.lati
+        };
+        var e_1 = {
+            long: currentPosition.long - u * normal.long,
+            lati: currentPosition.lati - u * normal.lati
+        };
+        var e_2 = {
+            long: currentPosition.long + u * normal.long,
+            lati: currentPosition.lati + u * normal.lati
+        };
+        drawArrow(p.lati, p.long, e_1.lati, e_1.long, e_2.lati, e_2.long);
     },
     // finish quest if correct qr code is scanned
-    finish: function(scanned) {
+    finish: function(scanned, currentTarget) {
         // compare scanned to current target id
         // if they are the same reset current target and add to history
+        if(scanned == currentTarget) {
+            return {success: True, points: TargetDB.targets[currentTarget].points};
+        }
+        return {success: False, points: 0};
     }
 };
 
@@ -56,7 +85,7 @@ var Game = {
         totalPoints: 0,
         currentRewards: new Array()
     },
-    start: function() {
+    start: function(duration) {
         // start game
     },
     gatherItems: function() {
