3a8292058e1eb4727c04c6d03f4712e4047a4d4b
Max Zeyen <m_zeyen09@cs.uni-kl.de>
2016-03-05 11:59:29 +0100

    game.js skeleton

--- www/js/game.js
+++ www/js/game.js
@@ -16,30 +16,25 @@ var TargetDB = {
     rewards: [
         {
             shopName: McDonalds,
-            descr: '50% Off a Happy Meal'
+            descr: '50% Off a Happy Meal',
+            points: 10
         },
         {
             shopName: Engelhorn,
-            descr: '5% Off'
+            descr: '5% Off',
+            points: 100
         }
     ]
 };
 
-var ProgressLog = {
-    currentTarget: 0,
-    targetHistory: new Array(),
-    currentPoints: 0,
-    totalPoints: 0
-};
-
 var Quest = {
     // assign a quest to the player
-    acquire: function(profile, currentPosition) {
+    acquire: function(duration, currentPosition) {
         // based on interests and available time select quest/target
         return target;
     },
     // track the position of the current quest
-    follow: function(target) {
+    follow: function(target, currentPosition) {
         // get direction to target based on current position
         // get current position
         // return normalized 2d direction vector
@@ -48,5 +43,26 @@ var Quest = {
     // finish quest if correct qr code is scanned
     finish: function(scanned) {
         // compare scanned to current target id
+        // if they are the same reset current target and add to history
+    }
+};
+
+
+var Game = {
+    log: {
+        currentTarget: 0,
+        targetHistory: new Array(),
+        currentPoints: 0,
+        totalPoints: 0,
+        currentRewards: new Array()
+    },
+    start: function() {
+        // start game
+    },
+    gatherItems: function() {
+        // gather shop items
+    },
+    buyItem: function(id) {
+        // buy item with given id
     }
 };
