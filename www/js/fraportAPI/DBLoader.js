8988cbc573fbfc66a7f5a5ca3bc60a55b548e2bc
Nils Feige <nfeige@rhrk.uni-kl.de>
2016-03-06 02:14:28 +0100

    New changelist

--- www/js/fraportAPI/DBLoader.js
+++ www/js/fraportAPI/DBLoader.js
@@ -27,37 +27,59 @@ function getTrainStationsByName(nameString) {
 function getDepartureTable(stationID, date, time) {
     var serviceURL = "/departureBoard";
     var departureTableData = "";
-    console.log($.ajax({
-        url: DB_Base_URL + serviceURL + defaultParameters + "id=" + stationID + "&date=" + date + "&time=" + time,
+    var urlString = DB_Base_URL + serviceURL + defaultParameters + "id=" + stationID + (date != undefined ? ("&date=" + date) : "") + ((time != undefined) ? "&time=" + time : "");
+    $.ajax({
+        url: urlString,
         headers : { authKey : authKey_ },
         dataType: "json",
         async: false,
         success: function (result) {
-            departureTableData = result;
-            console.log(result);
-            console.log("Successfully loaded trainStations for namestring: " + nameString);
+            departureTableData = result["DepartureBoard"]["Departure"];
+            console.log("Successfully loaded departure table for " + stationID + " and date " + date + " and time " + time);
         }
-    }));
+    });
     return departureTableData;
 }
 
 function getArrivalTable(stationID, date, time) {
     var serviceURL = "/arrivalBoard";
     var arrivalTableData = "";
-    console.log($.ajax({
-        url: DB_Base_URL + serviceURL + defaultParameters + "id=" + stationID + "&date=" + date + "&time=" + time,
+    var urlString = DB_Base_URL + serviceURL + defaultParameters + "id=" + stationID + (date != undefined ? ("&date=" + date) : "") + ((time != undefined) ? "&time=" + time : "");
+    $.ajax({
+        url: urlString,
         headers : { authKey : authKey_ },
         dataType: "json",
         async: false,
         success: function (result) {
-            arrivalTableData = result;
-            console.log(result);
-            console.log("Successfully loaded trainStations for namestring: " + nameString);
+            arrivalTableData = result["ArrivalBoard"]["Arrival"];
+            console.log("Successfully loaded arrival table for " + stationID + " and date " + date + " and time " + time);
         }
-    }));
+    });
     return arrivalTableData;
 }
 
-//getTrainStationsByName("Frankfurt Hbf");
-//getDepartureTable("008070003");
-//getArrivalTable("008070003");
+function getJourneyDetailsForArrivalOrDeparture(arrivalOrDeparture) {
+    var urlString = arrivalOrDeparture["JourneyDetailRef"]["ref"];
+    var journeyData = "";
+    console.log("URL STRING: " + urlString);
+    $.ajax({
+        url: urlString,
+        headers : { authKey : authKey_ },
+        dataType: "json",
+        async: false,
+        success: function (result) {
+            journeyData = result["JourneyDetail"];
+            console.log("Successfully loaded Journey details for journey" + arrivalOrDeparture);
+        }
+    });
+
+    return journeyData;
+}
+
+console.log("Departures");
+console.log(getDepartureTable("008070003"));
+console.log("Arrivals");
+console.log(getArrivalTable("008070003"));
+console.log("journeyData");
+console.log(getJourneyDetailsForArrivalOrDeparture(getArrivalTable("008070003")[0]));
+console.log(getJourneyDetailsForArrivalOrDeparture(getDepartureTable("008070003")[0]));
